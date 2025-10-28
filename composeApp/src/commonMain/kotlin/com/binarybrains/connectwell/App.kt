72adfee9e2b90ed5819766aa26e9b7f4bf63848d
Leon Raj <leonraj1606@gmail.com>
2025-10-29 01:08:34 +0530

    Added a simple chat bubble component.

--- composeApp/src/commonMain/kotlin/com/binarybrains/connectwell/App.kt
+++ composeApp/src/commonMain/kotlin/com/binarybrains/connectwell/App.kt
@@ -6,14 +6,14 @@ package com.binarybrains.connectwell
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
-import com.binarybrains.connectwell.presentation.homePage
+import com.binarybrains.connectwell.presentation.testPage
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Preview
 @Composable
 fun App() {
     MaterialTheme {
-        homePage()
+        testPage()
     }
 }
 
---
+++ composeApp/src/commonMain/kotlin/com/binarybrains/connectwell/presentation/component/simpleChatBubble.kt
@@ -0,0 +1,43 @@
+package com.binarybrains.connectwell.presentation.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+data class message(
+    val message: String,
+    val author: String
+)
+
+@Composable
+fun SimpleChatBubble(modifier: Modifier = Modifier) {
+
+    val msg = message("Hello there", "John")
+    Surface(modifier = modifier) {
+        Box(
+            modifier = Modifier
+                .padding(16.dp)
+                .background(
+                    color = MaterialTheme.colorScheme.primaryContainer,
+                    shape = RoundedCornerShape(16.dp)
+                )
+                .clip(RoundedCornerShape(16.dp)),
+        ) {
+            Text(
+                text = msg.message,
+                fontSize = 16.sp,
+                color = MaterialTheme.colorScheme.onPrimaryContainer,
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+    }
+}
